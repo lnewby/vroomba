ae0dd50387ae713daeebb59cecfd147b7122332f
Tiana <bulakul@usc.edu>
2019-05-28 22:40:24 -0700

    add multiple spawner feature

--- app/aframe-monster-system.js
+++ app/aframe-monster-system.js
@@ -1,64 +1,19 @@
 !function (e) {
 
-	AFRAME.registerComponent('spawning-zone', {
-
-		schema: {
-			spawnPosition: {type: 'vec3', default: {x: 0, y: 0.0001, z: 0}},
-			spawnRadius: {type: 'number', default: 1},
-			spawnPoolSize: {type: 'number', default: 5},
-			spawnRate: {type: 'number', default: 300}
-		},
-
-		init: function () {
-			this.registerSpawningZone();
-		},
-
-		registerSpawningZone: function() {
-			// Show spawning zone
-			
-			console.log("Spawning zone: creating...");
-
-			this.spawningZone = document.createElement('a-entity');
-			this.spawningZone.setAttribute('geometry', {
-	            primitive: 'circle',
-	            radius: this.data.spawnRadius
-        	});
-        	this.spawningZone.setAttribute('color', 'red');
-			this.spawningZone.setAttribute('position', this.data.spawnPosition);
-			this.spawningZone.setAttribute('rotation', {x: -90, y: 0, z: 0});
-			this.el.sceneEl.appendChild(this.spawningZone);
-
-			console.log("Spawning zone: created at " + JSON.stringify(this.data.spawnPosition));
-		}
-
-	});
-
-
 	AFRAME.registerComponent('monster' , {
 
 		schema: {
-			spawningZone: {type: 'selector', default: '#spawning-zone'},
-			health: {type: 'number', default: 50},
-			hitPoint: {type: 'number', default: 10}
+			health: {type: 'number', default: 50}
 		},
 
 		init: function () {
 			this.el.visible = false;
-			this.timer = 0;
 			this.system.registerMonster(this);
 		},
 
-		tick: function () {
-			if (this.timer == this.data.spawningZone.getAttribute('spawning-zone').spawnRate) {
-				this.system.spawn(this);
-				this.timer = 0;
-			}
-			this.timer++;
-		},
-
-		onHit: function() {
+		onHit: function(damagePoint) {
 			if (this.data.health > 0)
-				this.data.health -= this.data.hitPoint;
+				this.data.health -= damagePoint;
 		},
 
 		remove: function () {
@@ -71,69 +26,104 @@
 	AFRAME.registerSystem('monster', {
 		init: function () {
 			this.monsterPrefab;
-			this.monsters = {}
-			this.monsterCounter = 0;
 		},
 
 		registerMonster: function (e) {
 			console.log("Monster: cloning");
 			if (this.monsterPrefab = e.el.sceneEl.querySelector('#monster')) {
 				this.monsterPrefab.setAttribute('visible', false);
-				this.monsterPrefab.position = this.monsterPrefab.getAttribute('position');
 				console.log(this.monsterPrefab);
 			}
 		},
 
-		spawningCoordinates: function(spawnRadius) {
+		unregisterMonster: function () {
+		}
+
+	});
+
+
+	AFRAME.registerComponent('spawner', {
+
+		// Circular area where monsters should spawn
+
+		schema: {
+			spawnRadius: {type: 'number', default: 1},
+			spawnPoolSize: {type: 'number', default: 5},
+			spawnRate: {type: 'number', default: 300},
+
+			monster: {type: 'selector', default: '#monster'},
+			monsterType: {type: 'string'}
+		},
+
+		init: function () {
+			this.timer = 0;
+			this.monsterCounter = 1;
+			this.spawnPosition = this.el.object3D.position;
+			this.registerSpawningZone();
+			console.log(this.spawnPosition);
+		},
+
+		registerSpawningZone: function() {
+			// Show spawning zone
+			
+			console.log("Spawning zone: " + this.el.id + " creating...");
+
+			this.spawningZone = document.createElement('a-entity');
+			this.spawningZone.setAttribute('geometry', {
+	            primitive: 'circle',
+	            radius: this.data.spawnRadius
+        	});
+        	this.spawningZone.setAttribute('color', 'red');
+			this.spawningZone.setAttribute('position', this.spawnPosition);
+			this.spawningZone.setAttribute('rotation', {x: -90, y: 0, z: 0});
+			this.el.sceneEl.appendChild(this.spawningZone);
+
+			console.log("Spawning zone: created at " + JSON.stringify(this.spawnPosition));
+		},
+
+		tick: function () {
+			if (this.timer == this.data.spawnRate) {
+				this.spawn(this.data.monster);
+				this.timer = 0;
+			}
+			this.timer++;
+		},
+
+		spawningCoordinates: function() {
 			// Generate random coordinates in spawning zone
 			var a = Math.random() * 2 * Math.PI;
-			var r = spawnRadius * Math.sqrt(Math.random());
+			var r = this.data.spawnRadius * Math.sqrt(Math.random());
 
-			this.x = r * Math.cos(a);
-			this.y = this.monsterPrefab.position.y;
-			this.z = r * Math.sin(a);
+			this.x = (r * Math.cos(a)) + this.spawnPosition.x;
+			this.y = this.data.monster.getAttribute('position').y;
+			this.z = (r * Math.sin(a)) + this.spawnPosition.z;
 		},
 
 		spawn: function (e) {
 
-			if (this.monsterCounter < e.data.spawningZone.getAttribute('spawning-zone').spawnPoolSize) {
+			if (this.monsterCounter <= this.data.spawnPoolSize) {
 
-				console.group("Monster: spawning monster" + this.monsterCounter );
+				console.group("Monster: spawning monster " + this.monsterCounter + "/" + this.data.spawnPoolSize);
 
-				this.spawningCoordinates(e.data.spawningZone.getAttribute('spawning-zone').spawnRadius);
+				this.spawningCoordinates(this.data.spawnRadius);
 
 				console.log("Spawn position: {x: " + this.x + ", y:" + this.y + ", z:" + this.z + "}");
 
-				if (0) {
-					// Add monster as object 3D
-					var monster = new THREE.Object3D();
-					monster = this.monsterPrefab.object3D.clone();
-					monster.position.set(this.x, this.y, this.z);
-					monster.visible = true;
-					this.monsters[this.monsterCounter] = [];
-					this.monsters[this.monsterCounter].push(monster);
-					console.log(monster);
-					this.el.sceneEl.object3D.add(monster);
-				} else {
-					// Add monster as entity
-					var monster = this.monsterPrefab.cloneNode(true);
-					monster.removeAttribute('monster');
-					monster.setAttribute('monster-clone', '');
-					monster.setAttribute('id', 'monster' + this.monsterCounter);
-					monster.setAttribute('position', {x: this.x, y: this.y, z: this.z});
-					monster.setAttribute('follow', {target: '#player', speed: 0.2, space: 0.2});
-					monster.setAttribute('visible', true);
-					console.log(monster);
-					this.el.sceneEl.appendChild(monster);
-				}
+				// Add monster as entity
+				var monster = this.data.monster.cloneNode(true);
+				monster.removeAttribute('monster');
+				monster.setAttribute('monster-clone', '');
+				monster.setAttribute('id', 'MM_' + monster.object3D.uuid);
+				monster.setAttribute('position', {x: this.x, y: this.y, z: this.z});
+				monster.setAttribute('follow', {target: '#player', speed: 0.2, space: 0.2});
+				monster.setAttribute('visible', true);
+				console.log(monster);
+				this.el.sceneEl.appendChild(monster);
 
 				this.monsterCounter++;
 			
 				console.groupEnd();
 			}
-		},
-
-		unregisterMonster: function () {
 		}
 
 	});
