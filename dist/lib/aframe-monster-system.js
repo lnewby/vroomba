a92763757fa2c52f559d6522dc310ad88ed62504
Lance Newby <lnewby@gmail.com>
2019-06-25 01:33:35 -0700

    wip fix vive triggers to use raycasters in game

--- dist/lib/aframe-monster-system.js
+++ dist/lib/aframe-monster-system.js
@@ -57,6 +57,7 @@
 
                 var monster_id = 'MM_' + monster.object3D.uuid;
                 monster.setAttribute('id', monster_id);
+                monster.setAttribute('class', 'collidable');
                 monster.setAttribute('position', {x: loc.x, y: loc.y, z: loc.z});
                 monster.setAttribute('follow', {target: '#player', speed: 0.2, space: 0.5});
                 monster.setAttribute('visible', true);
